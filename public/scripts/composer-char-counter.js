43dfce80444b727055cccd23c1a56f17c0972e5a
Carlos Garate <carlos.m.garate@gmail.com>
2017-04-18 14:09:04 -0400

    Started creating styles and elements for the UI. Added the jquery functionality to count chars remaining in the tweet.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+
+// document ready.
+$(function () {
+  $( ".new-tweet textarea").on("keyup", function(event) {
+    var theTextArea = $( this );
+    var theCounter = $( this ).parent()[0].lastElementChild;
+    theCounter.innerText = 140 - theTextArea[0].textLength;
+    if (theCounter.innerText < 0) {
+      $(theCounter).css("color", "#ff0000");
+    } else {
+      $(theCounter).css("color", "#000000");
+    }
+  });
+
+})
+
